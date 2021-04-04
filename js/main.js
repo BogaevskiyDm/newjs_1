4fe1cf79deb49c208bb42b350663c27abc44701c
Dmitriy <bogaevskiy.dmitriy@mail.ru>
2021-04-04 19:49:11 +0300

    Homework #16

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+console.log('Sample JavaScript #1 HW #15');
 /*
  * #1
  *
@@ -131,3 +131,300 @@ console.log(strReplace);
 var myString = {str:'We are real developers'};
 console.log(myString.str.toUpperCase());
 console.log(myString.str.toLowerCase());
+
+console.log('Sample JavaScript #2 HW #16');
+
+/*
+ * #1
+ *
+ * Создайте объект userObj, описывающий человека.
+ *
+ * Следующие поля обязательны:
+ * firstName – любое имя, строка
+ * lastName – любая фамилия, строка
+ * age – любой возраст, число
+ */
+var userObj = {
+    firstName: 'Roma',
+    lastName: 'Dankov',
+    age: 21,
+    fullName() {
+    return `${userObj.firstName} ${userObj.lastName}`;
+}
+};
+
+/*
+ * #2
+ *
+ * Для объекта из п.1 создайте метод fullName, который будет возвращать полное имя,
+ * состоящее из firstName и lastName, склеенных в строку через пробел.
+ *
+ * Например:
+ * userObj.firstName ← 'Имя
+ * userObj.lastName ← Фамилия'
+ * userObj.fullName() → 'Имя Фамилия'.
+ */
+
+
+console.log(userObj.fullName());
+
+/*
+ * #3
+ *x
+ * Дана функция defUpperStr('My text'), которая возвращает текст, преобразованный в верхний регистр, т.е:
+ * defUpperStr('My text') → 'MY TEXT'.
+ *
+ * Если функция вызывается без параметра defUpperStr(), она не должна возвращать undefined,
+ * в этом случае требуется вернуть строку текста по умолчанию в верхнем регистре, т.е:
+ * defUpperStr() → 'DEFAULT TEXT'.
+ *
+ * При выполнении задачи не используйте оператор if, требуется решение с логическим оператором ||.
+ */
+
+/* eslint-disable capitalized-comments */
+function defUpperStr(str){
+    return (str || 'Default text').toUpperCase();
+}
+console.log(defUpperStr('My text')); // MY TEXT
+
+console.log(defUpperStr()); // DEFAULT TEXT
+
+/*
+ * #4
+ *
+ * Создайте функцию evenFn(n), которая принимает параметром число – количество итераций цикла,
+ * т.е. for 0..n. Функция должна вернуть массив, состоящий только из четных значений, генерируемых в цикле.
+ *
+ * Причем:
+ * 0 не должен попадать в результирующий массив
+ * цикл должен работать до n включительно
+ * разрешен только оператор for
+ *
+ * Например:
+ * evenFn(10) → [2, 4, 6, 8, 10]
+ * evenFn(15) → [2, 4, 6, 8, 10, 12, 14]
+ * evenFn(20) → [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+ */
+function evenFn(n){
+    var myArr = [];
+    for (let i = 1; i<=n; i++) if (i % 2 === 0) myArr.push(i);
+    return myArr;
+}
+
+console.log(evenFn(10)); // [2, 4, 6, 8, 10]
+console.log(evenFn(15)); // [2, 4, 6, 8, 10, 12, 14]
+console.log(evenFn(20)); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+/*
+ * #5
+ *
+ * Создайте функцию weekFn(n), которая принимает номер дня недели, а возвращает его название.
+ * Если вводится строка, любое дробное число или число вне диапазона 1..7 – функция должна вернуть null.
+ *
+ * Например:
+ * 1 → 'Понедельник'
+ * 2 → 'Вторник'
+ * ...
+ * 7 → 'Воскресенье'
+ *
+ * В реализации функции обязательно должны быть использованы операторы switch / case / default.
+ */
+
+// console.log(weekFn(1)); // 'Понедельник'
+
+// console.log(weekFn(3)); // 'Среда'
+
+// console.log(weekFn(7)); // 'Воскресенье'
+
+// console.log(weekFn(9)); // null
+
+// console.log(weekFn(1.5)); // null
+
+  // null
+function weekFn(n){
+    if(n==1) return ('Понедельник');
+    else if(n==2) return ('Вторник');
+    else if(n==3) return ('Среда');
+    else if(n==4) return ('Четверг');
+    else if(n==5) return ('Пятница');
+    else if(n==6) return ('Суббота');
+    else if(n==7) return ('Воскресенье');
+    else return(null);
+}
+console.log(weekFn(1));
+console.log(weekFn(2));
+console.log(weekFn(7));
+console.log(weekFn(3));
+console.log(weekFn(5));
+console.log(weekFn(1.5));
+console.log(weekFn('Jorik'));
+
+function weekFn(n){
+    let str = '';
+    switch (n) {
+    case 1: str = 'Понедельник';
+    break;
+    case 2: str = 'Вторник';
+    break;
+    case 3: str = 'Среда';
+    break;
+    case 4: str = 'Четверг';
+    break;
+    case 5: str = 'Пятница';
+    break;
+    case 6: str = 'Суббота';
+    break;
+    case 7: str = 'Воскресенье';
+    break;
+    default: str = null;
+    break; 
+    }
+    return str;
+}
+console.log(weekFn(1));
+console.log(weekFn(6));
+console.log(weekFn(7));
+console.log(weekFn(5));
+console.log(weekFn(4));
+console.log(weekFn(1.5));
+console.log(weekFn('Jorik'));
+/*
+ * #6
+ *
+ * создайте функцию ageClassification(n), которая будет в качестве параметра принимать любые числа
+ * и возвращать строку согласно следующим условиям, n:
+ *           менее 0 – null
+ *             0..24 – 'детский возраст'
+ *           24+..44 – 'молодой возраст'
+ *           44+..65 – 'средний возраст'
+ *           65+..75 – 'пожилой возраст'
+ *           75+..90 – 'старческий возраст'
+ *          90+..122 – 'долгожители'
+ *         более 122 – null
+ *
+ * При выполнении задания допускается использовать только тернарный оператор ?.
+ * Использование операторов if, switch – запрещено.
+ */
+
+// console.log('-1 :', ageClassification(-1)); // -1 : null
+
+// console.log('5 :', ageClassification(5)); // 5 : детский возраст
+
+// console.log('34 :', ageClassification(34)); // 34 : молодой возраст
+
+// console.log('50 :', ageClassification(50)); // 50 : средний возраст
+
+// console.log('65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
+
+// console.log('80 :', ageClassification(80)); // 80 : старческий возраст
+
+// console.log('110 :', ageClassification(110)); // 110 : долгожители
+
+// console.log('130 :', ageClassification(130)); // 130 : null
+
+
+
+  
+ 
+    function ageClassification(age){
+        return age > 0 ? age > 24 ? age > 44 ? age > 65 ? age > 75 ? age > 90 ? age > 122 ? null : 'долгожители' : 'старческий возраст' : 'пожилой возраст' : 'средний возраст' : 'молодой возраст' : 'детский возраст' : null;
+    }
+    
+    console.log('    -1 :', ageClassification(-1)); // -1 : null
+console.log('     1 :', ageClassification(1)); // 1 : детский возраст
+console.log('    24 :', ageClassification(24)); // 24 : детский возраст
+console.log(' 24.01 :', ageClassification(24.01)); // 24.01 : молодой возраст
+console.log('    44 :', ageClassification(44)); // 44 : молодой возраст
+console.log(' 44.01 :', ageClassification(44.01)); // 44.01 : средний возраст
+console.log('    65 :', ageClassification(65)); // 65 : средний возраст
+console.log('  65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
+console.log('    75 :', ageClassification(75)); // 75 : пожилой возраст
+console.log(' 75.01 :', ageClassification(75.01)); // 75.01 : старческий возраст
+console.log('    90 :', ageClassification(90)); // 90 : старческий возраст
+console.log(' 90.01 :', ageClassification(90.01)); // 90.01 : долгожители
+console.log('   122 :', ageClassification(122)); // 122 : долгожители
+console.log('122.01 :', ageClassification(122.01)); // 122.01 : null
+console.log('   150 :', ageClassification(150)); // 150 : null
+ /*
+ * #7
+ *
+ * Создайте функцию oddFn(n), которая принимает параметром число – количество итераций цикла.
+ * Функция должна вернуть массив, состоящий только из нечетных значений, генерируемых в цикле.
+ *
+ * Причем:
+ * 0 не должен попадать в результирующий массив
+ * цикл должен работать до n включительно
+ * разрешен только оператор while
+ *
+ * Например:
+ * oddFn(10) → [1, 3, 5, 7, 9]
+ * oddFn(15) → [1, 3, 5, 7, 9, 11, 13, 15]
+ * oddFn(20) → [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+ */
+ function oddFn(n) {
+    let arr = [];
+    let i = 0;
+    while (i++ < n) if (i % 2 == 1) arr.push(i);
+    return arr;
+}
+
+console.log(oddFn(10)); // [1, 3, 5, 7, 9]
+
+console.log(oddFn(15)); // [1, 3, 5, 7, 9, 11, 13, 15]
+
+console.log(oddFn(20)); // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+/*
+ * #8
+ *
+ * Создайте основную функцию mainFunc(a, b, func), которая принимает три параметра:
+ * a – число
+ * b - число
+ * func –  обрабатывающая параметры a и b, возвратная (callback) функция
+ *
+ * Реализуйте проверку: если третьим параметром передается не функция, нужно вернуть false.
+ *
+ */
+function mainFunc(a, b, cb){
+    if (cb && typeof cb === 'function') return cb(a, b);
+    return false;
+}
+/*
+ * реализуйте следующие функции, которые будут осуществлять механизм callback в основной функции,
+ * возвращая ей результат собственного вычисления...
+ * для возведения в степень и получения произвольного значения можете воспользоваться методами объекта Math.
+ */
+
+function cbRandom(a, b){
+    return Math.floor(Math.random() * (b - a + 1)) + a;
+}
+console.log(mainFunc(2, 5, cbRandom));
+// cbRandom(a, b) – вычисляет и возвращает произвольное целое число в диапазоне между a и b включительно.
+
+function cbPow(a , b){
+    return Math.pow(a, b);
+}
+console.log(mainFunc(2, 5, cbPow));
+// cbPow(a, b) – вычисляет и возвращает результат возведения числа a в степень b.
+
+function cbAdd (a, b){
+    return a + b;
+}
+console.log(mainFunc(2, 5, cbAdd))
+// cbAdd(a, b) – вычисляет и возвращает сумму двух чисел a и b.
+
+/*
+ * mainFunc() должна возвращать результат работы переданной ей возвратной функции, например:
+ * mainFunc(2, 5, cbRandom) → случайно от 2 до 5 включительно
+ * mainFunc(10, 30, cbRandom) → случайно 10..30 включительно
+ * mainFunc(2, 5, cbPow) → 32
+ * mainFunc(2, 5, cbAdd) → 7
+ * mainFunc(2, 5, 'not a func') → false
+ */
+
+// console.log(mainFunc(2, 5, cbRandom)); // целые числа в диапазоне 2..5
+
+// console.log(mainFunc(2, 5, cbPow)); // 32
+
+// console.log(mainFunc(2, 5, cbAdd)); // 7
+
+// console.log(mainFunc(2, 5, 'not a func')); // false
